1e45dc57be8c8a1b9095fbe439fe964be835c1a1
Soy <da5868@naver.com>
2026-04-16 03:23:51 +0900

    fix: btn은 id get으로 변경

--- week2/homework/budget/index.js
+++ week2/homework/budget/index.js
@@ -1,5 +1,5 @@
 // 헤더 - 좌측 아이콘 클릭시 새로고침
-const reloadIcon = document.querySelector(".reset-icn");
+const reloadIcon = document.getElementById('reload-icn');
 reloadIcon.addEventListener("click", (e) => {
     location.reload(true);
 });
