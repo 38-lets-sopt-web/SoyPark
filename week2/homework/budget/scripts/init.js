fc7094beea80f838051a578a22bf089b183ea875
Soy <da5868@naver.com>
2026-04-17 02:32:17 +0900

    feat: 템플릿 초기화 함수

---
+++ week2/homework/budget/scripts/init.js
@@ -0,0 +1,11 @@
+// 템플릿 초기화 함수
+export function initSelectOptions() {
+    const templateSelects = document.querySelectorAll('select[data-options-template]');
+    templateSelects.forEach(select => {
+        const templateId = select.getAttribute('data-options-template');
+        const template = document.getElementById(templateId);
+        if (template) {
+            select.appendChild(template.content.cloneNode(true));
+        }
+    });
+}
