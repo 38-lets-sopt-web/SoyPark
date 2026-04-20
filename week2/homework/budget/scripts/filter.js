2ff0401133de6e202b3e6ccbbec196b131777994
Soy <da5868@naver.com>
2026-04-20 17:11:53 +0900

    feat: 필터링

---
+++ week2/homework/budget/scripts/filter.js
@@ -0,0 +1,47 @@
+import { getStorageData } from "./storage.js";
+import { renderAllItems } from "./list.js";
+
+const applyFilter = () => {
+    const allData = getStorageData();
+    
+    // 현재 입력된 필터값
+    const nameValue = document.getElementById('filter-name').value;
+    const typeValue = document.getElementById('filter-type').value;
+    const categoryValue = document.getElementById('filter-category').value;
+    const paymentValue = document.getElementById('filter-payment').value;
+
+    // 필터링
+    const filteredData = allData.filter(item => {
+        const matchType = (typeValue === 'all') || 
+            (typeValue === '지출' && item.amount < 0) ||
+            (typeValue === '수익' && item.amount > 0);
+
+        // 제목이 포함되어 있거나 빈칸이면 통과
+        const matchName = item.title.includes(nameValue);
+
+        // 카테고리가 'all'이거나 값이 일치하면 통과
+        const matchCategory = (categoryValue === 'all' || item.category === categoryValue);
+        const matchPayment = 
+            (paymentValue === 'all' || 
+            (paymentValue === '카드' && item.payment.includes('카드')) ||  // 신용, 체크 다 포함
+            (item.payment === paymentValue));
+        
+        return matchName && matchType && matchCategory && matchPayment;
+    });
+
+    renderAllItems(filteredData);
+};
+
+// 적용 버튼
+const applyBtn = document.getElementById('btn-apply');
+applyBtn.addEventListener('click', (e) => {
+    e.preventDefault(); // 폼 제출 시 새로고침 방지
+    applyFilter();
+});
+
+// 초기화 버튼 
+const resetBtn = document.getElementById('btn-reset');
+resetBtn.addEventListener('click', () => {
+    document.querySelector('.filter-form').reset(); // 폼 비우기
+    renderAllItems(getStorageData()); 
+});
--- week2/homework/budget/scripts/index.js
+++ week2/homework/budget/scripts/index.js
@@ -3,6 +3,7 @@ import "./header.js";
 import "./modal.js";
 import { renderAllItems } from "./list.js";
 import { getStorageData } from "./storage.js";
+import "./filter.js";
 
 const data = getStorageData();
 renderAllItems(data);
