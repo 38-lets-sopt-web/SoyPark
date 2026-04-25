8c54087bb5143687f6979d5a35acd160246e5c9a
Soy <da5868@naver.com>
2026-04-25 16:03:55 +0900

    refacor: 검색 기능 커스텀 훅으로 분리

--- week3/practice/practice1/src/App.jsx
+++ week3/practice/practice1/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
 import { members } from "./member";
+import { useSearch } from "./hooks/useSearch";
 
 function App() {
-  const [search, setSearch] = useState("");
-  const [filteredMembers, setFilteredMembers] = useState(members); 
-
-  const handleSearch = (e) => {
-    setSearch(e.target.value);
-  };
-
-  // 버튼 클릭 이벤트 함수
-  const handleSearchClick = () => {
-    const result = members.filter((member) => member.name.includes(search));
-    setFilteredMembers(result); 
-  };
+// 커스텀 훅 사용!
+  const { search, filteredData, handleSearchChange, handleSearchClick } =
+    useSearch(members);
 
   return (
     <>
       <Header />
       <Search 
         search={search} 
-        onSearchChange={handleSearch} 
+        onSearchChange={handleSearchChange} 
         onSearchClick={handleSearchClick}
       />
       <section style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
-        {filteredMembers.map((member) => (
+        {filteredData.map((member) => (
           <Card key={member.id} {...member} />
         ))}
       </section>
@@ -37,3 +28,4 @@ function App() {
 }
 
 export default App;
+
