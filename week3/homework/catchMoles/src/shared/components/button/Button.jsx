c6c244a1305af9eaac27d75254371e689db5008f
Soy <da5868@naver.com>
2026-05-01 01:44:02 +0900

    feat: 버튼 컴포넌트 구현

---
+++ week3/homework/catchMoles/src/shared/components/button/Button.jsx
@@ -0,0 +1,36 @@
+const Button = ({ 
+  color = "blue", 
+  isActive = false, 
+  onClick, 
+  children, 
+  className = "", 
+  ...props 
+}) => {
+
+  const colorStyles = {
+    blue: {
+      active: "bg-blue-400 border-blue-400 text-white hover:bg-blue-300",
+      inactive: "bg-transparent border-blue-400 text-blue-400 hover:bg-blue-300"
+    },
+    red: "bg-red-500 hover:bg-red-600 border-red-500",
+    green: "bg-green-500 hover:bg-green-600 border-green-500",
+    gray: "bg-gray-500 hover:bg-gray-600 border-gray-500",
+  };
+
+  const currentColor = isActive ? colorStyles[color].active : colorStyles[color].inactive;
+
+  const baseStyles = "items-center rounded-full px-2 py-1 text-white text-sm border-2";
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`${baseStyles} ${colorStyles[color]} ${currentColor} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
