5b4d466f99d00ab227f6a571bd3a387a71951ae9
Soy <da5868@naver.com>
2026-05-01 03:33:21 +0900

    chore: 게임 관련 상수 선언

---
+++ week3/homework/catchMoles/src/pages/game/constants/gameSetting.js
@@ -0,0 +1,18 @@
+export const GAME_SETTINGS = {
+  DEFAULT_LIMIT_TIME: 15, 
+  HOLE_COUNT: 4, // 2x2 보드
+  DOG_SHOW_DURATION: 1000, 
+  SUCCESS_DURATION: 700,
+};
+
+export const HOLE_STATUS = {
+  EMPTY: 'empty',
+  SLEEPY: 'sleepy', 
+  ANGRY: 'angry',  
+  HAPPY: 'happy',  
+};
+
+export const SCORE_CHANGE = {
+  SUCCESS: 1,
+  FAIL: -1,
+};
