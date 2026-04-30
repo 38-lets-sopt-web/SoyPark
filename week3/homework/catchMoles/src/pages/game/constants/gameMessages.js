9b3976dc52c94a15c0c693e58d2670e4cdb19e04
Soy <da5868@naver.com>
2026-05-01 04:04:12 +0900

    chore: game message 수정

--- week3/homework/catchMoles/src/pages/game/constants/gameMessages.js
+++ week3/homework/catchMoles/src/pages/game/constants/gameMessages.js
@@ -2,5 +2,5 @@ export const GAME_MESSAGES = {
   INTRO: '졸린 강아지를 깨워보세요! 사나운 강아지는 조심하세요.',
   GAME_OVER: '게임 종료',
   SUCCESS: '강아지를 깨웠다.',
-  FAIL: '꽝!! 사나운 강아지를 건들였다..',
+  FAIL: '꽝!! 사나운 강아지를 건들였다.',
 };
