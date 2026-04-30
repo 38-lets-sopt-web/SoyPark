fcdf6f86f7e57183375da1d575e83b96ffd91911
Soy <da5868@naver.com>
2026-05-01 03:53:14 +0900

    feat: 점수, 메시지 표시 및 성공시 이미지 변경까지

--- week3/homework/catchMoles/src/pages/game/GamePage.jsx
+++ week3/homework/catchMoles/src/pages/game/GamePage.jsx
@@ -1,22 +1,23 @@
-import { useState } from "react";
 import Button from "../../shared/components/button/Button";
 import Hole from "./components/Hole";
 import { useGame } from "./hooks/useGame";
 
 const progressStyle = {
     container: "flex flex-col items-center justify-center p-3 gap-1 rounded-lg bg-blue-100",
-    title: "text-sm",
-    number: "text-xl"
+    title: "text-sm underline",
+    number: "text-2xl"
 }
 
 const GamePage = () => {
-    const [holeStates, setHoleStates] = useState(["empty", "sleepy", "angry", "empty"]);
-
     const 
     {
         timeLeft,
-        isPlaying,
+        score,
+        successCount,
+        failCount,
         gameMessage,
+        holeStates,
+        handleHoleClick,
         gameStart,
     } = useGame();
 
@@ -30,19 +31,19 @@ const GamePage = () => {
                 </div>
                 <div className={`${progressStyle.container} col-span-2 py-6`}>
                     <p className={progressStyle.title}>총 점수</p>
-                    <p className={progressStyle.number}>0</p>
+                    <p className={progressStyle.number}>{score}</p>
                 </div>
                 <div className={progressStyle.container}>
                     <p className={`${progressStyle.title} text-green-500`}>성공</p>
-                    <p className={progressStyle.number}>0</p>
+                    <p className={progressStyle.number}>{successCount}</p>
                 </div>
                 <div className={progressStyle.container}>
                     <p className={`${progressStyle.title} text-pink-500`}>실패</p>
-                    <p className={progressStyle.number}>0</p>
+                    <p className={progressStyle.number}>{failCount}</p>
                 </div>
                 <div className={`${progressStyle.container} col-span-2 pt-5 pb-8`}>
                     <p className={`${progressStyle.title}`}>안내 메시지</p>
-                    <p className="text-xs">안내메시지 자리입니다.안내메시지 자리입니다.안내메시지 자리입니다.</p>
+                    <p className="text-xs text-center mt-1">{gameMessage}</p>
                 </div>
             </div>
 
@@ -64,7 +65,7 @@ const GamePage = () => {
                             <Hole 
                             key={index} 
                             status={status} 
-                            // onClick={() => handleHoleClick(index)} 
+                            onClick={() => handleHoleClick(index)} 
                             />
                         ))}
                 </div>
--- week3/homework/catchMoles/src/pages/game/hooks/useGame.js
+++ week3/homework/catchMoles/src/pages/game/hooks/useGame.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { GAME_SETTINGS } from "../constants/gameSetting"
+import { GAME_SETTINGS, HOLE_STATUS } from "../constants/gameSetting"
 import { useRef } from "react";
 import { GAME_MESSAGES } from "../constants/gameMessages";
 
@@ -7,7 +7,21 @@ export const useGame = () => {
     const [timeLeft, setTimeLeft] = useState(GAME_SETTINGS.DEFAULT_LIMIT_TIME);
     const [isPlaying, setIsPlaying] = useState(false);
     const timerRef = useRef(null);
-    const [gameMessage, setGameMessage] = useState("");
+    const [gameMessage, setGameMessage] = useState(GAME_MESSAGES.INTRO);
+
+    const [score, setScore] = useState(0);
+    const [successCount, setSuccessCount] = useState(0);
+    const [failCount, setFailCount] = useState(0); 
+    const [holeStates, setHoleStates] = useState(Array(4).fill('empty'));
+
+    const showDog = () => {
+        const randomIndex = Math.floor(Math.random() * 4);
+
+        const newHoles = Array(4).fill('empty');
+        newHoles[randomIndex] = 'sleepy';
+        setHoleStates(newHoles);
+    }
+
 
     const gameStart = () => {
         if(isPlaying) return;
@@ -15,11 +29,18 @@ export const useGame = () => {
         setIsPlaying(true);
         setTimeLeft(GAME_SETTINGS.DEFAULT_LIMIT_TIME);
 
+        setScore(0);
+        setSuccessCount(0);
+        setFailCount(0);
+
+        showDog();
+
         timerRef.current = setInterval(() => {
             setTimeLeft((prev) => {
                 if (prev <= 1 ) {
                     clearInterval(timerRef.current);
                     setIsPlaying(false);
+                    setHoleStates(Array(4).fill(HOLE_STATUS.EMPTY));
                     setGameMessage(GAME_MESSAGES.GAME_OVER);
                     return 0;
                 }
@@ -28,10 +49,40 @@ export const useGame = () => {
         }, 1000);
     }
 
+    const handleHoleClick = (index) => {
+        if (!isPlaying) return;
+
+        if (holeStates[index] === HOLE_STATUS.SLEEPY){
+            setScore((prev) => prev + 1);
+            setSuccessCount((prev) => prev + 1);
+            setGameMessage(GAME_MESSAGES.SUCCESS);
+
+            const successHoles = [...holeStates];
+            successHoles[index] = HOLE_STATUS.WAKE_UP;
+            setHoleStates(successHoles);
+
+            setTimeout(() => {
+                showDog();
+            }, GAME_SETTINGS.DOG_SHOW_DURATION);
+
+        } else if (holeStates[index] === HOLE_STATUS.ANGRY) { // 꽝 클릭시
+            setScore((prev) => prev - 1);
+            setFailCount((prev) => prev + 1);
+            setGameMessage(GAME_MESSAGES.FAIL);
+            
+            showDog();
+            }
+    }
+
     return {
         timeLeft,
         isPlaying,
         gameMessage,
+        score,
+        successCount,
+        failCount,
+        holeStates,
         gameStart,
+        handleHoleClick,
     }
 }
