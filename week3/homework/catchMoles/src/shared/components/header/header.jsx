f473e70b9da840238af455d0c25fa5de7fd50a10
Soy <da5868@naver.com>
2026-05-01 01:45:09 +0900

    style: header에 marginBottom 추가

--- week3/homework/catchMoles/src/shared/components/header/header.jsx
+++ week3/homework/catchMoles/src/shared/components/header/header.jsx
@@ -2,7 +2,7 @@ import Button from "../button/Button";
 
 const Header = ({ activeTab, onClick }) => {
 return (
-    <header className="flex items-center text-center gap-4 bg-blue-200 p-4 rounded-lg">
+    <header className="flex items-center text-center gap-4 bg-blue-200 p-4 rounded-lg mb-8">
         <h2 className="font-bold">졸린 강아지를 깨워보세요!</h2>
         <div className="flex gap-1">
             <Button
