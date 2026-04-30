de8b89f4e53488050fed990877e2e7970655c77f
Soy <da5868@naver.com>
2026-05-01 02:04:19 +0900

    chore: header 태그 수정

--- week3/homework/catchMoles/src/shared/components/header/header.jsx
+++ week3/homework/catchMoles/src/shared/components/header/header.jsx
@@ -3,7 +3,7 @@ import Button from "../button/Button";
 const Header = ({ activeTab, onClick }) => {
 return (
     <header className="flex items-center text-center gap-4 bg-blue-200 p-4 rounded-lg mb-8">
-        <h2 className="font-bold">졸린 강아지를 깨워보세요!</h2>
+        <h1 className="font-bold">졸린 강아지를 터치해 깨워보세요!</h1>
         <div className="flex gap-1">
             <Button
                 isActive={activeTab === 'game'}
