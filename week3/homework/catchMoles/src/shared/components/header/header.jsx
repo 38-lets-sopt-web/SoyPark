4349aab0930a9a019338d483b11b4deca82bb491
Soy <da5868@naver.com>
2026-05-01 02:31:33 +0900

    style: header와 main section 간격 조정

--- week3/homework/catchMoles/src/shared/components/header/header.jsx
+++ week3/homework/catchMoles/src/shared/components/header/header.jsx
@@ -2,7 +2,7 @@ import Button from "../button/Button";
 
 const Header = ({ activeTab, onClick }) => {
 return (
-    <header className="flex items-center text-center gap-4 bg-blue-200 p-4 rounded-lg mb-8">
+    <header className="flex items-center text-center gap-4 bg-blue-200 p-4 rounded-lg mb-4">
         <h1 className="font-bold">졸린 강아지를 터치해 깨워보세요!</h1>
         <div className="flex gap-1">
             <Button
