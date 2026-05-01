981f7252ed89f66338d7282d501a04bcd790add6
Soy <da5868@naver.com>
2026-05-02 01:06:07 +0900

    refactor: 게임 진행, 보드 컴포넌트 분리

--- week3/homework/catchMoles/src/pages/game/GamePage.jsx
+++ week3/homework/catchMoles/src/pages/game/GamePage.jsx
@@ -1,13 +1,9 @@
-import Button from "../../shared/components/button/Button";
+import GameProgress from "./components/Board";
 import GameoverModal from "./components/GameoverModal";
-import Hole from "./components/Hole";
+import GameBoard from "./components/GameProgress";
 import { useGame } from "./hooks/useGame";
 
-const progressStyle = {
-    container: "flex flex-col items-center justify-center p-3 gap-1 rounded-lg bg-blue-100",
-    title: "text-sm underline",
-    number: "text-2xl"
-}
+const level = 1;
 
 const GamePage = () => {
     const 
@@ -20,6 +16,7 @@ const GamePage = () => {
         holeStates,
         showModal,
         resetTime,
+        isPlaying,
         handleHoleClick,
         gameStart,
         gameStop,
@@ -29,58 +26,29 @@ const GamePage = () => {
         <>
             <section className="flex rounded-lg bg-blue-200 p-4 gap-3">
                 {/* 진행 상황 */}
-                <div className="grid grid-cols-2 gap-2 w-45 rounded-lg">
-                    <div className={`${progressStyle.container} col-span-2 py-6`}>
-                        <p className={progressStyle.title}>남은 시간</p>
-                        <p className={progressStyle.number}>{timeLeft}</p>
-                    </div>
-                    <div className={`${progressStyle.container} col-span-2 py-6`}>
-                        <p className={progressStyle.title}>총 점수</p>
-                        <p className={progressStyle.number}>{score}</p>
-                    </div>
-                    <div className={progressStyle.container}>
-                        <p className={`${progressStyle.title} text-green-500`}>성공</p>
-                        <p className={progressStyle.number}>{successCount}</p>
-                    </div>
-                    <div className={progressStyle.container}>
-                        <p className={`${progressStyle.title} text-pink-500`}>실패</p>
-                        <p className={progressStyle.number}>{failCount}</p>
-                    </div>
-                    <div className={`${progressStyle.container} col-span-2 pt-5 pb-6`}>
-                        <p className={`${progressStyle.title}`}>안내 메시지</p>
-                        <p className="text-xs text-center mt-1">{gameMessage}</p>
-                    </div>
-                </div>
+                <GameProgress 
+                    timeLeft={timeLeft}
+                    score={score}
+                    successCount={successCount}
+                    failCount={failCount}
+                    gameMessage={gameMessage}
+                />
 
                 {/* 게임 보드 */}
-                <div className={`${progressStyle.container} w-full flex-1`}>
-                    <div className="section-header w-full p-1">
-                        <select className="bg-white/30 border-none rounded-lg px-1 py-1.5 text-xs font-semibold shadow-sm outline-none cursor-pointer">
-                            <option value="1">Level 1</option>
-                            <option value="2"></option>
-                            <option value="3">Level 3</option>
-                        </select>
-                        <div className="flex gap-1">
-                            <Button color="green" onClick={gameStart}>시작</Button>
-                            <Button color="pink" onClick={gameStop}>중단</Button>
-                        </div>
-                    </div>
-                        <div className="grid grid-cols-2 gap-6 w-100 bg-slate-100 p-8 rounded-2xl">
-                            {holeStates.map((status, index) => (
-                                <Hole 
-                                    key={index} 
-                                    status={status} 
-                                    onClick={() => handleHoleClick(index)} 
-                                />
-                            ))}
-                    </div>
-                    
-                </div>
+                <GameBoard
+                    level={level}
+                    // handleLevelChange={handleLevelChange}
+                    isPlaying={isPlaying}
+                    gameStart={gameStart}
+                    gameStop={gameStop}
+                    holeStates={holeStates}
+                    handleHoleClick={handleHoleClick}
+                />
             </section>
 
             {showModal && (
                 <GameoverModal
-                    level={1}
+                    level={level}
                     score={score}
                     resetTime={resetTime}
                 />
