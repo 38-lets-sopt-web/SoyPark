00a9a92638369a1d7342a24cf6d4afbe4ba76754
Soy <da5868@naver.com>
2026-05-02 15:14:09 +0900

    feat: 라우터 실습

---
+++ week4/login-practice/src/router/Router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import MemberDetail from "../pages/MemberDetail";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+  },
+  {
+    path: "/member/:name",
+    Component: MemberDetail,
+  },
+]);
+
+export default router;
