8543ad7032a519298f7ae6ab70e439300f5b1fe4
Soy <da5868@naver.com>
2026-05-08 02:45:15 +0900

    feat: 로그인 페이지 UI 구현

--- week4/homework/member/src/pages/login/LoginPage.tsx
+++ week4/homework/member/src/pages/login/LoginPage.tsx
@@ -1,9 +1,40 @@
+import TextField from "@components/textField/TextField";
+import { useState } from "react";
+import * as styles from "./Login.css";
+import Button from "@components/button/Button";
+import { useNavigate } from "react-router-dom";
+
 const LoginPage = () => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleGoToSignup = () => {
+    navigate("/signup");
+  };
+
+  // 버튼 비활성화 조건
+  const isNextDisabled = !id || !password;
+
   return (
-    <>
-      로그인페이지
-      <h1>네..</h1>
-    </>
+    <div className={styles.wrapper}>
+      <h1 className={styles.header}>SOPT MEMBERS</h1>
+      <div className={styles.container}>
+        <TextField label="아이디" type="text" value={id} onChange={setId} />
+        <TextField
+          label="비밀번호"
+          type="password"
+          value={password}
+          onChange={setPassword}
+        />
+      </div>
+      <div className={styles.btnContainer}>
+        <Button disabled={isNextDisabled}>로그인</Button>
+        <button type="button" className={styles.btn} onClick={handleGoToSignup}>
+          회원가입
+        </button>
+      </div>
+    </div>
   );
 };
 
