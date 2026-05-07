72ab913713a970ee3ca3331330e90f3587a25091
Soy <da5868@naver.com>
2026-05-08 06:14:59 +0900

    feat: 마이페이지 UI 구현 및 아이디, 파트 불러오기

---
+++ week4/homework/member/src/pages/mypage/MyPage.css.ts
@@ -0,0 +1,20 @@
+import { vars } from "@styles/theme.css";
+import { style } from "@vanilla-extract/css";
+
+export const wrapper = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "3rem",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+});
+
+export const editArea = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "2rem",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "80%",
+});
--- week4/homework/member/src/pages/mypage/MyPage.tsx
+++ week4/homework/member/src/pages/mypage/MyPage.tsx
@@ -1,9 +1,48 @@
+import MyCard from "@/pages/mypage/components/myCard/MyCard";
+import * as styles from "./MyPage.css";
+import Title from "@components/title/Title";
+import type { ResponseUserInfo } from "@pages/mypage/types/mypage";
+import { getUserInfo } from "@apis/getUserInfo";
+import { LOCAL_STORAGE_KEY } from "@constants/key";
+import { useState, useEffect } from "react";
+import { PART_OPTIONS } from "@/pages/signup/constants/partOption";
+import PartSelect from "@components/textField/PartSelect";
+import TextField from "@components/textField/TextField";
+import useSignupForm from "@pages/signup/hooks/useSignupForm";
+import Button from "@components/button/Button";
+
 const MyPage = () => {
+  const [userInfo, setUserInfo] = useState<ResponseUserInfo>();
+  const { getInputProps } = useSignupForm();
+
+  useEffect(() => {
+    const userId = Number(localStorage.getItem(LOCAL_STORAGE_KEY.userID));
+
+    if (userId) {
+      getUserInfo(userId).then((data) => {
+        setUserInfo(data);
+      });
+    }
+  }, []);
+
+  const data = userInfo?.data;
+
   return (
-    <>
-      마이페이지
-      <h1>네..</h1>
-    </>
+    <div className={styles.wrapper}>
+      <Title title="내 정보" />
+      <MyCard id={data?.loginId || ""} part={data?.part || ""} />
+      <section className={styles.editArea}>
+        <TextField label="이름" {...getInputProps("name")} />
+        <TextField label="이메일" {...getInputProps("email")} />
+        <TextField label="나이" {...getInputProps("age")} />
+        <PartSelect
+          label="파트"
+          options={PART_OPTIONS}
+          {...getInputProps("part")}
+        />
+        <Button>정보 수정</Button>
+      </section>
+    </div>
   );
 };
 
