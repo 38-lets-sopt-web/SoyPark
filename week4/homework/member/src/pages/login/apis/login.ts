05a44e0390b97ef678a81401fd515137ec697357
Soy <da5868@naver.com>
2026-05-08 05:59:10 +0900

    fix: login 응답 타입 수정

--- week4/homework/member/src/pages/login/apis/login.ts
+++ week4/homework/member/src/pages/login/apis/login.ts
@@ -1,7 +1,7 @@
-import type { RequestLogin } from "../types/login";
+import type { RequestLogin, ResponseLogin } from "../types/login";
 import { axiosInstance } from "@apis/axiosInstance";
 
-export const postLogin = async (body: RequestLogin): Promise<RequestLogin> => {
+export const postLogin = async (body: RequestLogin): Promise<ResponseLogin> => {
   const { data } = await axiosInstance.post("/api/v1/auth/signin", body);
 
   return data;
