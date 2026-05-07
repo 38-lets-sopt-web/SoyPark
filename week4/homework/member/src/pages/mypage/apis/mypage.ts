7943f12ddb167a899fc6e7dea7f30cbd65f8a901
Soy <da5868@naver.com>
2026-05-08 05:43:54 +0900

    feat: 마이페이지 api 스펙 작성

---
+++ week4/homework/member/src/pages/mypage/apis/mypage.ts
@@ -0,0 +1,18 @@
+import type { ResponseUsers, ResponseUserInfo } from "../types/mypage";
+import { axiosInstance } from "@apis/axiosInstance";
+
+// 전체 유저 정보 조회
+export const getUsers = async (): Promise<ResponseUsers> => {
+  const { data } = await axiosInstance.get(`/api/v1/users`);
+
+  return data;
+};
+
+// 정보 수정
+export const patchUserInfo = async (
+  userId: number,
+): Promise<ResponseUserInfo> => {
+  const { data } = await axiosInstance.patch(`/api/v1/users/${userId}`);
+
+  return data;
+};
