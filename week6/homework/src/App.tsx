cdb5a2129a36e57c028dcb07e5e90424bc2651b1
Soy <da5868@naver.com>
2026-06-02 00:37:38 +0900

    chore: app에 라우터 설정 추가

--- week6/homework/src/App.tsx
+++ week6/homework/src/App.tsx
@@ -1,5 +1,12 @@
+import router from "@routes/router";
+import { RouterProvider } from "react-router";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
