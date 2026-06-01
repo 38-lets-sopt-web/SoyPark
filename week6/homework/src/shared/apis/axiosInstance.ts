e15570872e6738fc07bdc319327dc4cde6647549
Soy <da5868@naver.com>
2026-06-02 02:02:33 +0900

    chore: axios Instance 설정 및 엔드포인트 상수화

---
+++ week6/homework/src/shared/apis/axiosInstance.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosError } from "axios";
+
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const getGuestSessionId = () => {
+  if (typeof window === "undefined") return null;
+
+  return window.localStorage.getItem("guest_session_id");
+};
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const guestSessionId = getGuestSessionId();
+
+  config.params = {
+    ...config.params,
+    api_key: config.params?.api_key ?? apiKey,
+    ...(guestSessionId && {
+      guest_session_id: config.params?.guest_session_id ?? guestSessionId,
+    }),
+  };
+
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
