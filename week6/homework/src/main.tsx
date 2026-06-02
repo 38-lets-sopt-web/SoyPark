be56b16b51f5d30cc4f98d5e550ea975ca601fbe
Soy <da5868@naver.com>
2026-06-02 21:28:07 +0900

    chore: styles 폴더 shared 안으로 이동

--- week6/homework/src/main.tsx
+++ week6/homework/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
+import "@styles/index.css";
 import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
