ca51013822e554a4e0996a690679fbe11e75ff94
Soy <da5868@naver.com>
2026-06-02 18:00:06 +0900

    feat: 드롭다운 컴포넌트

--- week6/homework/src/pages/movieList/MovieListPage.tsx
+++ week6/homework/src/pages/movieList/MovieListPage.tsx
@@ -3,13 +3,20 @@ import { useMovieListQuery } from "./apis/getMovieList";
 import ListCard from "./components/card/ListCard";
 import { useNavigate } from "react-router-dom";
 import { ROUTE_PATH } from "@constants/path";
+import RatingFilter from "./components/ratingFilter/RatingFilter";
+import { useState } from "react";
 
 const MovieListPage = () => {
+  const [selectedRating, setSelectedRating] = useState("all");
   const navigate = useNavigate();
   const { data: movieList, isPending, isError } = useMovieListQuery();
 
   if (isPending) return <div>로딩중</div>;
   if (isError) return <div>에러 발생</div>;
+
+  const handleRatingChange = (value: string) => {
+    setSelectedRating(value);
+  };
 
   const handleCardClick = (movieId: number) => {
     navigate(ROUTE_PATH.movieDetail(movieId));
@@ -18,7 +25,9 @@ const MovieListPage = () => {
   return (
     <div className="mx-auto flex min-h-screen w-full max-w-7xl flex-col gap-5 px-6 py-10 sm:px-10 lg:px-12">
       <h1 className="text-3xl font-bold">Movie Explorer</h1>
-      <section>별점 필터링</section>
+      <section>
+        <RatingFilter value={selectedRating} onChange={handleRatingChange} />
+      </section>
       <section className="grid grid-cols-[repeat(auto-fit,minmax(220px,260px))] gap-6">
         {movieList?.results.map((movie) => (
           <ListCard
