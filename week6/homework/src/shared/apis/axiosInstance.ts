2dbf7c79c7aa9adfa6433b2eed41d80b406e0696
Soy <da5868@naver.com>
2026-06-02 19:26:26 +0900

    fix: instance에서 params에 아이디 붙이지 않도록

--- week6/homework/src/shared/apis/axiosInstance.ts
+++ week6/homework/src/shared/apis/axiosInstance.ts
@@ -1,12 +1,6 @@
 import axios, { AxiosError } from "axios";
 
 const apiKey = import.meta.env.VITE_API_KEY;
-const GUEST_SESSION_KEY = "guest_session_id";
-
-// 로컬 스토리지에서 게스트 세션 ID 가져오기
-const getStoredGuestSessionId = () => {
-  return localStorage.getItem(GUEST_SESSION_KEY);
-};
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -17,14 +11,9 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const guestSessionId = getStoredGuestSessionId();
-
   config.params = {
     ...config.params,
     api_key: config.params?.api_key ?? apiKey,
-    ...(guestSessionId && {
-      guest_session_id: config.params?.guest_session_id ?? guestSessionId,
-    }),
   };
 
   return config;
